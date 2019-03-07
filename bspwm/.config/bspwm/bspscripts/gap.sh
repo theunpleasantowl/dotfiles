0aa1605fdc22b96cc5f438ad40fa9fdf7a727789
Hibiki <theunpleasantowl@gmail.com>
2019-03-07 00:03:02 -0500

    incremental gaps

--- bspwm/.config/bspwm/bspscripts/gap.sh
+++ bspwm/.config/bspwm/bspscripts/gap.sh
@@ -1,11 +1,19 @@
 #!/bin/bash
 
-GAP=/tmp/bspwmgap
-
-if [ ! -f $GAP ]; then
-	bspc config window_gap 0
-	touch $GAP
-else
-	bspc config window_gap 12
-	rm $GAP
-fi
+case "$1" in
+	tog)
+		if [ 0 -lt $((`bspc config window_gap`)) ]; then
+			bspc config window_gap 0
+		else
+			bspc config window_gap 12
+		fi
+		;;
+	dec)
+		if [ 0 -ne $((`bspc config window_gap`)) ]; then
+			bspc config window_gap $(( `bspc config -d focused window_gap` - 3))
+		fi
+		;;
+	inc)
+		bspc config window_gap $(( `bspc config -d focused window_gap` + 3))
+		;;
+esac
